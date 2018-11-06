134d5e1388afb79f729545399110ffe501d9baf6
Artem Kapitonenko <a.kapitonenko@itransition.com>
2018-11-06 19:44:19 +0300

    Add imgs

--- src/containers/FrameSelectionContainer.tsx
+++ src/containers/FrameSelectionContainer.tsx
@@ -1,22 +1,66 @@
+import * as React from 'react';
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 
 import { ApplicationState } from '../store';
 
-import { openSearchPanel, closeSearchPanel } from '../store/searchPanel/actions';
+import * as searchPanelActions from '../store/searchPanel/actions';
+import * as framesActions from '../store/frames/actions';
 
-import FrameSelection from '../components/FrameSelection';
+import SearchPanel from '../components/SearchPanel';
 
 import '../styles/frameSelection.css';
+
+interface PropsFromState {
+  open: boolean;
+  anchor: any;
+  list: any;
+}
+
+interface PropsFromDispatch {
+  handleOpen: typeof searchPanelActions.openSearchPanel;
+  handleClose: typeof searchPanelActions.closeSearchPanel;
+  handleFetch: any;
+}
+
+type AllProps = PropsFromState & PropsFromDispatch;
 
 const mapStateToProps = (state: ApplicationState) => ({
   open: state.searchPanel.open,
   anchor: state.searchPanel.anchor,
+  list: state.frames.list
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-  handleOpen: (element: any) => dispatch(openSearchPanel(element.target)),
-  handleClose: () => dispatch(closeSearchPanel()),
+  handleOpen: (element: any) => dispatch(searchPanelActions.openSearchPanel(element.target)),
+  handleClose: () => dispatch(searchPanelActions.closeSearchPanel()),
+  handleFetch: () => dispatch(framesActions.fetchFrames()),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(FrameSelection)
+class FrameSelectionContainer extends React.Component<AllProps> {
+  componentDidMount() {
+    const { handleFetch } = this.props;
+
+    handleFetch();
+  }
+
+  render() {
+    const { open, anchor, handleOpen, handleClose } = this.props;
+
+    return (
+      <div className="page__wrapper yellow-section">
+        <div className="main-content">
+          <h1 className="page__title">Tailored Frame Selection</h1>
+          <section className="frame-selection__form">
+            <h2 className="frame-selection__form-title">Please enter up to five frame UPC's to check for compatibility and select the one that best suits the patient</h2>
+            <input className="frame-selection__form-input" type="text" onClick={handleOpen} />
+          </section>
+
+          <SearchPanel open={open} anchorEl={anchor} handleClose={handleClose} />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FrameSelectionContainer)
--- src/store/frames/actions.ts
+++ src/store/frames/actions.ts
@@ -1,6 +1,21 @@
+import { Dispatch } from 'redux';
 import { action } from 'typesafe-actions';
+
 import { FramesActionTypes, Frame } from './types';
+
+import { frames } from '../../test/frames';
 
 export const fetchRequest = () => action(FramesActionTypes.FETCH_REQUEST);
 export const fetchSuccess = (list: Frame[]) => action(FramesActionTypes.FETCH_SUCCESS, list);
 export const fetchError = (message: string) => action(FramesActionTypes.FETCH_ERROR, message);
+
+export const fetchFrames: any = () => (dispatch: Dispatch) => {
+  dispatch(fetchRequest());
+
+  return new Promise((resolver) => {
+    resolver(frames)
+  })
+  .then((frames: any) => {
+    dispatch(fetchSuccess(frames));
+  })
+}
